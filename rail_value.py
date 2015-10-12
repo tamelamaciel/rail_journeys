8edb84d17c1c4b92e02ef35bb1b599d3e29956be
Tamela Maciel <tamela.maciel@gmail.com>
2015-10-12 14:20:39 +0100

    populate main with code from old fun_radio_calc.py script

--- rail_value.py
+++ rail_value.py
@@ -35,6 +35,69 @@ def calc_value_ratio(start):
 def main():
     """Main entry point for the script."""
 
+	#read in distance data:
+	#TODO: if dist_data in namespace, use that; otherwise load data from file.
+	dist_data=np.genfromtxt('distances_from_'+start+'.txt',delimiter=', ',dtype=[('name', 'S30'), ('code', 'S4'), ('pc', 'S9'), ('lat', '<f8'), ('long', '<f8'), ('dist', '<f8')])
+
+	#read in price data:
+	rail_value=[]
+	file=open('prices_from_'+start+'.txt','r')
+
+	while 1:
+		line=file.readline()
+		if '#' in line:
+			continue
+		if not line: break
+
+		items=string.split(line,',')
+		
+		station_name=items[0]
+		station_code=items[1]
+		price=items[2].strip('\n')
+
+		#get distance data:
+
+		#index of postcode in dist_data
+		i=np.where(dist_data['code']==station_code)[0][0]
+
+		station_postcode=dist_data['pc'][i]
+		station_lat=dist_data['lat'][i]
+		station_long=dist_data['long'][i]
+		dist_miles=dist_data['dist'][i]
+		if dist_miles<700.:
+			try:
+				value_ratio=dist_miles/float(price)
+			except ValueError:
+				value_ratio=0
+
+		rail_value.append([station_name,station_code,station_postcode,station_lat,station_long,dist_miles,price,value_ratio])
+
+		nextline=string.join([station_name,station_code,station_postcode,str(station_lat),str(station_long),str(dist_miles),price,str(value_ratio)+'\n'],', ')
+
+		print nextline
+
+		fileout = open(file_name,'a')
+		fileout.write(nextline)
+		fileout.close()	
+
+	#sort rail_value array according to value_ratio
+	from operator import itemgetter
+	rail_value.sort(key=itemgetter(7))
+
+	best_value_name = rail_value[-1][0]
+	best_value_dist = rail_value[-1][5]
+	best_value_price = rail_value[-1][6]
+
+	print '---------------------------'
+	print 'Best value for money:  '
+	print '   (for single ticket)'
+	print '   Station: '+best_value_name
+	print '   Dist (miles): '+str(best_value_dist)
+	print '   Price (pounds): '+str(best_value_price)
+	print '---------------------------'
+
+	file.close()
+
 
 if __name__ == '__main__':
     sys.exit(main())
