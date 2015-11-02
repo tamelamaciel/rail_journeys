abcdd9db1cd0157e6797380f4969071deafb9411
Tamela Maciel <tamela.maciel@gmail.com>
2015-11-02 17:44:40 +0000

    convert code to use pandas for i/o, merging, and sorting

--- rail_value.py
+++ rail_value.py
@@ -1,8 +1,9 @@
 """Script to find the best value train tickets (miles per pence) in the UK"""
-
+import ipdb
 import sys
 import string
 import math
+import pandas as pd
 import numpy as np
 import os.path
 
@@ -13,68 +14,44 @@ import os.path
 #   default time of day: from 5am onwards
 # -----------------------------------------------------------------------
 # starting station, full name or three-letter code
-start = 'LEI' 
+start = 'CBG' 
 # date DD/MM/YY
-date = '09/11/15'
+date = '09-11-15'
 #===============================================================================
 
-day = date.split('/')[0]
-month = date.split('/')[1]
-year = date.split('/')[2]
+day = date.split('-')[0]
+month = date.split('-')[1]
+year = date.split('-')[2]
 
 def get_prices_from_national_rail(start,day,month,year,journey):
     """Gets ticket prices from national rail website for all UK journeys starting at *start*"""
+    price_data=[]
+    # TODO: write price at NaN if not found, rather than ??
+    # TODO: write file with no spaces
+    # TODO: don't need station codes
     return price_data
 
 def calc_distances_to_stations(start):
     """Calculates distance as the crow flies from starting point to all other station postcodes in statute miles."""
-    
+    dist_data=[]
+    # TODO: write file with no spaces
     return dist_data
 
 def calc_value_ratio(dist_data,price_data):
 	"""Calculates the best value journeys from *start* using prices and distances to each UK station"""
-    # initialize output file for rail value data
+	
+
+	#combine distance and price data into one data frame
+	rail_value=pd.merge(dist_data, price_data, on='station_name', how='left')
+
+	#add a new column, value_ratio = dist/price if the distance from start is < 700 miles (sanity check for UK)
+	rail_value['value_ratio'] = rail_value.apply(lambda x : x['distance_miles']/x['price'] if x['distance_miles'] <700. else NaN, axis=1)
+
+	#write to file
 	file_name='rail_value_'+start+'_'+date+'.csv' 
-	fileout = open(file_name,'w')
-	fileout.write('station_name,station_code,station_postcode,lat,long,dist_miles,price,miles_per_pound\n')
-	fileout.close()
+	rail_value.to_csv(file_name, sep=',')
 
-	rail_value=[]
-
-	for line in price_data:
-		line=file.readline()
-		if '#' in line:
-			continue
-		items=string.split(line,',')
-		
-		station_name=items[0]
-		station_code=items[1]
-		price=items[2].strip('\n')
-
-		#index of postcode in dist_data
-		i=np.where(dist_data['code']==station_code)[0][0]
-
-		station_postcode=dist_data['pc'][i]
-		station_lat=dist_data['lat'][i]
-		station_long=dist_data['long'][i]
-		dist_miles=dist_data['dist'][i]
-		if dist_miles<700.:
-			try:
-				value_ratio=dist_miles/float(price)
-			except ValueError:
-				value_ratio=0
-
-		rail_value.append([station_name,station_code,station_postcode,station_lat,station_long,dist_miles,price,value_ratio])
-
-		nextline=string.join([station_name,station_code,station_postcode,str(station_lat),str(station_long),str(dist_miles),price,str(value_ratio)+'\n'],', ')
-
-		print nextline
-
-		fileout = open(file_name,'a')
-		fileout.write(nextline)
-		fileout.close()	
-
-		return rail_value
+	return rail_value
 
 def main():
 	"""Main entry point for the script. 
@@ -83,38 +60,39 @@ def main():
 	#read in distance data:
 	#don't rerun distances function if data file already exists
 	if os.path.exists('distances_from_'+start+'.csv'):
-		dist_data=np.genfromtxt('distances_from_'+start+'.csv',delimiter=',',dtype=[('name', 'S30'), ('code', 'S4'), ('pc', 'S9'), ('lat', '<f8'), ('long', '<f8'), ('dist', '<f8')])
+		dist_data=pd.read_csv('distances_from_'+start+'.csv')
+		print 'distance data already exists for this station. skipping distance calc...'
 	else:
 		dist_data=calc_distances_to_stations(start)
 
 	#read in price data:
 	if os.path.exists('prices_from_'+start+'_'+date+'.csv'):
-		#file=open('prices_from_'+start+'_'+date_'.csv','r')
-		price_data=np.genfromtxt('prices_from_'+start+'_'+date+'.csv',delimiter=',')
-
+		price_data=pd.read_csv('prices_from_'+start+'_'+date+'.csv')
+		#drop unncessary station code column. data will be merged using station name as a key
+		price_data = price_data.drop('station_code', 1)
+		print 'price data already exists for this station and date. skipping web query...'
 	else:
 		price_data=get_prices_from_national_rail(start,day,month,year)
 
 
 	rail_value = calc_value_ratio(dist_data,price_data)
 
-	#sort rail_value array according to value_ratio
-	from operator import itemgetter
-	rail_value.sort(key=itemgetter(7))
+	#get journey with maximum value_ratio
+	best_value=rail_value.loc[rail_value['value_ratio'].idxmax()]
 
-	best_value_name = rail_value[-1][0]
-	best_value_dist = rail_value[-1][5]
-	best_value_price = rail_value[-1][6]
+	best_value_name = best_value['station_name']
+	best_value_code = best_value['station_code']
+	best_value_dist = best_value['distance_miles']
+	best_value_price = best_value['price']
 
 	print '---------------------------'
 	print 'Best value for money:  '
 	print '   (for single ticket)'
 	print '   Station: '+best_value_name
+	print '   Code: '+best_value_code
 	print '   Dist (miles): '+str(best_value_dist)
 	print '   Price (pounds): '+str(best_value_price)
 	print '---------------------------'
-
-	file.close()
 
 
 if __name__ == '__main__':
